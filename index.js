5c83be728d3c21a40cf7990023d765023dd1fd01
Rohit ranjan <ranjanrohit812@gmail.com>
2022-02-06 01:15:27 +0530

    Procfile bug fix

--- index.js
+++ index.js
@@ -1,14 +1,261 @@
-const express = require("express")
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express()
+const port = process.env.PORT || 3000;
 
-// use the express-static middleware
-app.use(express.static("public"))
+app.use(cors());
 
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+var questions = [
+  {
+    "id": 1,
+    "question": "How often do you travel by air in a year ?",
+    "options": ['I fly rarely', 'Occasionally', 'Regularly', 'Never'],
+  },
+  {
+    "id": 2,
+    "question": "What describes your diet ?",
+    "options": [
+      'Vegan',
+      'Vegetarian',
+      'Pescetarian',
+      'Ocassionaly eat meat',
+      'Regularly eat meat'
+    ],
+  },
+  {
+    "id": 3,
+    "question": "How much do you travel by car ?",
+    "options": [
+      'I don\'t drive',
+      '~5000 km',
+      '~ 5,000 - 10000 km',
+      '~ 10,000 - 15,000 km',
+      '>15,000 km'
+    ],
+  },
+  {
+    "id": 4,
+    "question": "Which kind of fuel do you use ?",
+    "options": ['Electric', 'Natural gas', 'Petrol, Disel'],
+  },
+  {
+    "id": 5,
+    "question": "Tell us something about your shopping habits ?",
+    "options": ['Rarely', 'Average', 'Shopper', 'Luxary Shopper'],
+  },
+  {
+    "id": 6,
+    "question": "How big is your home ?",
+    "options": [
+      'Sharing',
+      'One-Bedroom',
+      'Two-bedroom',
+      'Three-bedroom',
+      'Bunglow'
+    ],
+  },
+  {
+    "id": 7,
+    "question": "How many people live in your home ?",
+    "options": ['Just me', '2 people', '3 people', '4-6 people', ' > 7 people'],
+  },
+  {
+    "id": 8,
+    "question": "Which pet do you own ?",
+    "options": ['None', 'Cat', 'Dog', 'Other'],
+  },
+  {
+    "id": 9,
+    "question": "Do you use renewable energy sources at your home ?",
+    "options": ['Yes', 'Not Yet', 'Not Sure'],
+  },
+];
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  //res.json(docs);
+  res.json(questions);
+});
+
+app.post('/calculate', (req, res) => {
+
+  var carbonEmission = 2.33;
+
+  var responses = req.body;
+
+  for (var i = 0; i < Object.keys(responses).length; i++) {
+    if( Object.keys(responses).length == 0) {
+      break;
+    }
+    if (i == 0) {
+      switch (responses[i]) {
+        case 0:
+          carbonEmission += 0.66;
+          break;
+        case 1:
+          carbonEmission += 3.74;
+          break;
+        case 2:
+          carbonEmission += 12.37;
+          break;
+        case 3:
+          carbonEmission += 0;
+          break;
+        default:
+          break;
+      }
+    } else if (i == 1) {
+      switch (responses[i]) {
+        case 0:
+          carbonEmission -= 0.23;
+          break;
+        case 1:
+          carbonEmission -= 0.16;
+          break;
+        case 2:
+          carbonEmission -= 0.15;
+          break;
+        case 3:
+          carbonEmission -= 0.06;
+          break;
+        case 4:
+          carbonEmission += 0.08;
+          break;
+        default:
+          break;
+      }
+    } else if (i == 2) {
+      switch (responses[i]) {
+        case 0:
+          carbonEmission -= 0.42;
+          break;
+        case 1:
+          carbonEmission += 0.07;
+          break;
+        case 2:
+          carbonEmission += 1.04;
+          break;
+        case 3:
+          carbonEmission += 2.03;
+          break;
+        case 4:
+          carbonEmission += 3.49;
+          break;
+        default:
+          break;
+      }
+    } else if (i == 3) {
+      switch (responses[i]) {
+        case 0:
+          carbonEmission -= 0.18;
+          break;
+        case 1:
+          carbonEmission -= 0.04;
+          break;
+        case 2:
+          carbonEmission += 0.0;
+          break;
+        default:
+          break;
+      }
+    } else if (i == 4) {
+      switch (responses[i]) {
+        case 0:
+          carbonEmission += 0.18;
+          break;
+        case 1:
+          carbonEmission += 0.0;
+          break;
+        case 2:
+          carbonEmission += 3.26;
+          break;
+        case 3:
+          carbonEmission += 6.85;
+          break;
+        default:
+          break;
+      }
+    } else if (i == 5) {
+      switch (responses[i]) {
+        case 0:
+          carbonEmission -= 0.29;
+          break;
+        case 1:
+          carbonEmission -= 0.16;
+          break;
+        case 2:
+          carbonEmission -= 0.04;
+          break;
+        case 3:
+          carbonEmission += 0.09;
+          break;
+        case 4:
+          carbonEmission += 1.69;
+          break;
+        default:
+          break;
+      }
+    } else if (i == 6) {
+      switch (responses[i]) {
+        case 0:
+          carbonEmission += 0.54;
+          break;
+        case 1:
+          carbonEmission += 0.0;
+          break;
+        case 2:
+          carbonEmission -= 0.19;
+          break;
+        case 3:
+          carbonEmission -= 0.33;
+          break;
+        case 4:
+          carbonEmission -= 0.39;
+          break;
+        default:
+          break;
+      }
+    } else if (i == 7) {
+      switch (responses[i]) {
+        case 0:
+          carbonEmission -= 0.05;
+          break;
+        case 1:
+          carbonEmission += 0.25;
+          break;
+        case 2:
+          carbonEmission += 0.35;
+          break;
+        case 3:
+          carbonEmission += 0.11;
+          break;
+        default:
+          break;
+      }
+    } else if (i == 8) {
+      switch (responses[i]) {
+        case 0:
+          carbonEmission -= 1.1;
+          break;
+        case 1:
+          carbonEmission += 0.05;
+          break;
+        case 2:
+          carbonEmission += 0.0;
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  carbonEmission = carbonEmission.toFixed(2);
+
+  var result = { "result": carbonEmission };
+  res.send(result);
+});
+
+app.listen(port, () => console.log(`Carbon Footprint app listening on port ${port}!`));
